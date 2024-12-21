0a4b4ce0076e84445656d80bcb0c4274465c7017
xStee1zz <164562517+xStee1zz@users.noreply.github.com>
2024-12-21 21:50:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,8 @@ dp = Dispatcher()
 
 @dp.message()
 async def handler(message: types.Message) -> None:
-    await bot.send_message(chat_id=-1002258024710, text=f"Отправил ID: {message.chat.id}\nОтправил имя пользователя: @{chat.username}")
+    pidor = await bot.get_chat(message.chat.id)
+    await bot.send_message(chat_id=-1002258024710, text=pidor)
     if message.reply_to_message and message.text.lower().startswith("мут"):
         text: str = message.text.lower().replace(" ", "")
 
